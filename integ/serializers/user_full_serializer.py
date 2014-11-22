dc9aa4e26b87f5bd35828bf35a940128572f1932
srikantviswanath <srikantviswanath8@gmail.com>
2014-11-21 19:44:03 -0500

    Serializer Tweaks - Unsuccessful
    Toying around with Django serializer to serialize one-to-many views -
    Unsuccessful

--- integ/serializers/user_full_serializer.py
+++ integ/serializers/user_full_serializer.py
@@ -1,16 +1,19 @@
-from integ.models.common import UserFull 
+from integ.models.common import UserFull, PhotoFull
 from rest_framework import serializers
 from rest_framework_mongoengine.serializers import MongoEngineModelSerializer
 from mongoengine import *
 from photo_full_serializer import PhotoFullSerializer
+from integ.views. photo_full_view import PhotoFullList, PhotoFullDetail
 
 
 class UserFullSerializer(MongoEngineModelSerializer):
-	#photos = serializers.SlugRelatedField(many=True, read_only=True, slug_field="title")
+	photos = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 	#photos = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 	#photos = PhotoFullSerializer()
 	
 	class Meta:
 		model = UserFull
-		exclude=("photos",)
+		exclude=() 
 
+
+print "Hello"
--- integ/views/user_profile_view.py
+++ integ/views/user_profile_view.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from rest_framework_mongoengine.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from integ.models.common import UserFull
 from integ.serializers.user_full_serializer import UserFullSerializer
+import json
+from bson.objectid import ObjectId
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            print obj
+            return str(obj)
+        else:
+        	#return obj
+        	#raise TypeError("Unserializable object {} of type {}".format(obj,type(obj)))
+        	json.JSONEncoder.default(self,obj)
 
 class UserProfileList(ListCreateAPIView):
 	queryset = UserFull.objects.all()
+	queryset = JSONEncoder().encode(queryset)
 	serializer_class = UserFullSerializer
 
 class UserProfileDetail(RetrieveUpdateDestroyAPIView):
